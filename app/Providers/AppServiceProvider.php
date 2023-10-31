378f8894c2a507a4a89a386097e6196f7f436002
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-10-31 11:26:33 +0100

    Add uncompromised Password rule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $rule->uncompromised();
+        });
     }
 }
